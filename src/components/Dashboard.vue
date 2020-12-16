8d8f444defe1bcfe2d95292ff863a1a51cc04d1e
Sebastian Moreno <jhoansebml@gmail.com>
2020-12-16 17:21:14 -0500

    actualización de front

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,26 +1,37 @@
 <template>
     <div id="Dashboard">
-        <h1>Hola {{usuario}}, en este momento tu balance neto es de VALOR</h1>
+        <h2>Hola {{user}}</h2>
+        <h2>En este momento tus movimientos dan un neto de ${{netoUsuario}}</h2>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     name: "Dashboard",
     data: function(){
         return{
-            usuario: "none"
+            user: localStorage.getItem('current_user'),
+            netoUsuario: 0,
         }
     },
 
     created: function(){
-        this.usuario=localStorage.getItem('current_user')
+        let mivariable=this;
+        axios.get("http://localhost:8000/user/dashboard/" + this.user )
+        .then(response =>{
+            mivariable.netoUsuario=response.data.total
+        })
+        .catch((error)=>{
+            alert("error al acceder a la DB")
+        })
     }
 }
 </script>
 
 <style>
-#Dashboard h1{
+#Dashboard h2{
     text-align: center;
 }
 </style>
